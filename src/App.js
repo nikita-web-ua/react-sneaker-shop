49ccc48d6772e7998d0c46e60f0c1cc60e4d5bee
nikita-web-ua <maliarchuk99@gmail.com>
2022-05-17 11:27:18 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+          <div className="leftHeader">
+              <img width={40} height={40} src={'./img/logo.png'} alt="logo"/>
+              <div className="headerInfo">
+                  <h2>React Sneaker Shop</h2>
+                  <p>Best sneaker shop</p>
+              </div>
+          </div>
+          <ul className={'rightHeader'}>
+              <li>
+                  <img  src="./img/cart.svg" alt={'cart-logo'}/>
+                  <p>120 usd</p>
+              </li>
+              <li>
+                  <img src="./img/user.svg" alt={'user-logo'}/>
+              </li>
+          </ul>
+          
       </header>
+      <div className="content">
+          <div className="contentTop">
+              <h1>All Sneakers</h1>
+              <input type="text" placeholder={'Search'}/>
+          </div>
+          <div className="products">
+              <div className="item">
+                  <img width={133} height={112} src="./img/products/1.png" alt="nike sneakers"/>
+                  <h5>Nike Blazer Mid Suede</h5>
+                  <div className={'item-bottom'}>
+                      <div className={'item-price'}>
+                          <span>Price:</span>
+                          <b>120$</b>
+                      </div>
+                      <button className={'add-btn'}>
+                          <img width={11} height={11} src="./img/plus.svg" alt="Plus"/>
+                      </button>
+                  </div>
+              </div>
+              <div className="item">
+                  <img width={133} height={112} src="./img/products/1.png" alt="nike sneakers"/>
+                  <h5>Nike Blazer Mid Suede</h5>
+                  <div className={'item-bottom'}>
+                      <div className={'item-price'}>
+                          <span>Price:</span>
+                          <b>120$</b>
+                      </div>
+                      <button className={'add-btn'}>
+                          <img width={11} height={11} src="./img/plus.svg" alt="Plus"/>
+                      </button>
+                  </div>
+              </div>
+              <div className="item">
+                  <img width={133} height={112} src="./img/products/1.png" alt="nike sneakers"/>
+                  <h5>Nike Blazer Mid Suede</h5>
+                  <div className={'item-bottom'}>
+                      <div className={'item-price'}>
+                          <span>Price:</span>
+                          <b>120$</b>
+                      </div>
+                      <button className={'add-btn'}>
+                          <img width={11} height={11} src="./img/plus.svg" alt="Plus"/>
+                      </button>
+                  </div>
+              </div>
+              <div className="item">
+                  <img width={133} height={112} src="./img/products/1.png" alt="nike sneakers"/>
+                  <h5>Nike Blazer Mid Suede</h5>
+                  <div className={'item-bottom'}>
+                      <div className={'item-price'}>
+                          <span>Price:</span>
+                          <b>120$</b>
+                      </div>
+                      <button className={'add-btn'}>
+                          <img width={11} height={11} src="./img/plus.svg" alt="Plus"/>
+                      </button>
+                  </div>
+              </div>
+              <div className="item">
+                  <img width={133} height={112} src="./img/products/1.png" alt="nike sneakers"/>
+                  <h5>Nike Blazer Mid Suede</h5>
+                  <div className={'item-bottom'}>
+                      <div className={'item-price'}>
+                          <span>Price:</span>
+                          <b>120$</b>
+                      </div>
+                      <button className={'add-btn'}>
+                          <img width={11} height={11} src="./img/plus.svg" alt="Plus"/>
+                      </button>
+                  </div>
+              </div>
+              <div className="item">
+                  <img width={133} height={112} src="./img/products/1.png" alt="nike sneakers"/>
+                  <h5>Nike Blazer Mid Suede</h5>
+                  <div className={'item-bottom'}>
+                      <div className={'item-price'}>
+                          <span>Price:</span>
+                          <b>120$</b>
+                      </div>
+                      <button className={'add-btn'}>
+                          <img width={11} height={11} src="./img/plus.svg" alt="Plus"/>
+                      </button>
+                  </div>
+              </div>
+          </div>
+
+      </div>
+
     </div>
   );
 }
